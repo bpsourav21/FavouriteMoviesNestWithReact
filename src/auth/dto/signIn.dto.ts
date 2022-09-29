214cf045342f05bea23c96c72a76f9ae358038b7
Mahadi Hasan Sourav <sourav@chaldal.net>
2022-09-29 18:52:06 +0600

    add validation

--- src/auth/dto/signIn.dto.ts
+++ src/auth/dto/signIn.dto.ts
@@ -1,4 +1,12 @@
+import { IsEmail, IsNotEmpty, MinLength, MaxLength,  } from 'class-validator';
+
 export class SignInDto {
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+
+    @IsNotEmpty()
+    @MinLength(4)
+    @MaxLength(20)
     password: string;
 }
--- src/auth/dto/signUp.dto.ts
+++ src/auth/dto/signUp.dto.ts
@@ -1,5 +1,6 @@
 import { SignInDto } from "./signIn.dto";
-
+import { IsNotEmpty } from 'class-validator';
 export class SignUpDto extends SignInDto {
+    @IsNotEmpty()
     name: string;
 }
